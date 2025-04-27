87f02ae7a82ce86a274763c308e1a4249eeb4982
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 <hymavathi.lekkala@fissionlabs.com>
2025-04-27 12:47:30 +0530

    addedthefolder

---
+++ readdirectory.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+echo "Enter directory name:"
+read dir_name
+
+if [ ! -d "$dir_name" ]; then
+  mkdir "$dir_name"
+  echo "Directory '$dir_name' created."
+else
+  echo "Directory '$dir_name' already exists."
+fi
+
